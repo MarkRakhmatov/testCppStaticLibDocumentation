37307f0f8060af25cc78f977db6fe94bf03eccf2
Mark Rakhmatov <mark.rakhmatov@gmail.com>
2026-02-01 02:21:49 +0200

    Initial commit

---
+++ include/CppStaticLib/CppStaticLib.hpp
@@ -0,0 +1,61 @@
+#pragma once
+#include <string>
+
+/*! \mainpage CppStaticLib
+ *
+ * \section intro_sec Introduction
+ *
+ * This project simplify creation of C++ static libraries and provides basic tooling.
+ *
+ */
+
+/*! \brief Example namespace that contains static library functions
+ *
+ *    *  Detailed description of namesace
+ */
+namespace csl {
+  /*! \brief Example function that returns string.
+   *
+   *    *  Detailed description of function
+   *
+   *  \return hardcoded string
+   */
+  std::string getString();
+
+  /*! \brief Example compile time factorial function
+   *
+   *    *  Detailed description of function
+   *
+   *  \param input: integer
+   *  \return factorial of input
+   */
+  constexpr int factorialConstexpr(int input) noexcept {
+    if (input < 2) {
+      return 1;
+    }
+
+    return input * factorialConstexpr(input - 1);
+  }
+
+  /*! \brief Example runtime time factorial function
+   *
+   *    *  Detailed description of function
+   *
+   *  \param input: integer
+   *  \return factorial of input
+   */
+  int factorial(int input) noexcept;
+
+  /*! \brief Example of function not covered with test
+   *
+   *    * Testing is important for quality of software.
+   *    * This function left uncovered just to simplify usage of this project as template.
+   *    * CI configured to require 60% code coverage and not allows decrease of coverage.
+   *    * If your project has no tests you can temporarily change CI,
+   *    see .github/workflows/ci.yml 'Coverage - Check' fail_min and fail_diff_decrease options
+   *
+   *  \param value: integer value
+   *  \return some integer value
+   */
+  int uncoveredFunction(int value) noexcept;
+}  // namespace csl
---
+++ src/CppStaticLib.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include "CppStaticLib/CppStaticLib.hpp"
+
+
+namespace csl {
+
+  /*!
+   * \brief max input value that will not cause 32 bit integer overflow
+   *
+   * issue was found using fuzz test
+   */
+  constexpr int gMaxIntFactorialInput = 12;
+
+  std::string getString() { return "cpp static lib example"; }
+
+  int factorial(int input) noexcept {
+    if (input > gMaxIntFactorialInput) {
+      return -1;
+    }
+
+    if (input < 2) {
+      return 1;
+    }
+
+    return input * factorial(input - 1);
+  }
+
+  int uncoveredFunction(int value) noexcept {
+    if(value > 0) {
+      return 1;
+    }
+
+    return -1;
+  }
+
+}  // namespace csl
